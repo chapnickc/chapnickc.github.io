7053b2970130cc2b6342e9ad15419bc23c642bad
Chad Chapnick <chapnickc@gmail.com>
2020-04-04 10:04:57 -0500

    Updates

---
+++ precache-manifest.4f9866b2dcd9347b9aaed0e63e8497b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c1287d0f71e52bd3004687bb5ce04ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a97b9b829c2a7127aa9d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b1261fa90ddb02041604",
+    "url": "/static/js/2.61642f08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.61642f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97b9b829c2a7127aa9d",
+    "url": "/static/js/main.6e4b794c.chunk.js"
+  },
+  {
+    "revision": "568b7ff76ef79c0e7fe6",
+    "url": "/static/js/runtime-main.c1c4a6ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
