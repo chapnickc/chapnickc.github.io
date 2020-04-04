7336625c40ee2400eab9833c7abc9c97986cc077
Chad Chapnick <chapnickc@gmail.com>
2020-04-04 10:52:20 -0500

    added basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/Menu.js';
 
-/*        <a*/
-          //className="App-link"
-          //href="https://reactjs.org"
-          //target="_blank"
-          //rel="noopener noreferrer"
-        //>
-          //Learn React
-        /*</a>*/
-//<p> Edit <code>src/App.js</code> and save to reload.  </p>
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Container fluid className="App">
+      <Row>
+        <Col md={true}>
+          <Menu/>
+
+          <Container>
+            <p>Built using React
+            <img src={logo} className="App-logo" alt="logo" />
+            </p>
+          </Container>
+        </Col>
+
+        <Col className="App-main-content">
+          <Container>
+            <p>Some jibber jabber</p>
+          </Container>
+        </Col>
+
+      </Row>
+
+    </Container>
   );
 }
 
---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+export default class Menu extends React.Component {
+  render(){
+    return (
+      <Container>
+        <h1>Chad Chapnick</h1>
+      </Container>
+    );
+  }
+
+};
